ac2d1c2e376b7ced3eec31afe64614b023d1246e
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2026-04-21 21:38:09 -0300

    Guardas, data,  equipos chicos

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,57 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+data Categoria = Menor | Adulto | Jubilado deriving (Show)
+
+categoria :: Number -> Categoria
+categoria edad
+    | edad < 18 = Menor
+    | edad < 65 = Adulto
+    | otherwise = Jubilado
+
+------------------------------------
+
+--Se sabe que un club de fútbol tiene un nombre, cantidad de títulos (todos los títulos valen lo mismo tanto una final intercontinental contra el real madrid como una primera nacional) y sus jugadores. De los jugadores conocemos su nombre, su edad, su valor de mercado. Se pide modelar los equipos, los jugadores y las siguientes funciones:
+
+-- Así se crea un data con "record syntax"
+data Club = UnClub {
+    nombre :: String,
+    cantTitulos :: Number,
+    jugadores :: [Jugador]
+} deriving (Show)
+
+data Jugador = UnJugador {
+    nombreJug :: String,
+    edad :: Number,
+    valorMercado :: Number
+} deriving (Show)
+
+alf :: Jugador
+alf = UnJugador { nombreJug="Alf", edad=39, valorMercado=(-4) }
+
+boca :: Club
+boca = UnClub { nombre="Boca", cantTitulos=74, jugadores=[alf] }
+
+nacionalPotosi :: Club
+nacionalPotosi = UnClub { nombre="Nacional Potosí", cantTitulos=60, jugadores=[] }
+
+-- la función identidad devuelve lo que recibe
+identidad :: a -> a -- Variables de tipo. Me permiten que mi función reciba más de un tipo
+identidad x = x
+
+-- comprarJugador :: asdfsadf -> Jugador -> asdfasdf
+
+-- 1. esMasGrandeQue recibe dos clubes y dice si el primero es más grande que el segundo. La grandeza de un club se mide en base a cuantos títulos tiene.
+
+esMasGrandeQue :: Club -> Club -> Bool
+esMasGrandeQue clubGrande clubChico = cantTitulos clubGrande > cantTitulos clubChico
+
+-- 2. salirCampeon recibe un club y le suma uno a su cantidad de títulos.
+
+-- Como no hay efecto, necesito construir un nuevo club parecido, pero con un título más.
+salirCampeon :: Club -> Club
+-- salirCampeon club = UnClub { nombre=nombre club, cantTitulos=cantTitulos club + 1, jugadores=jugadores club }
+salirCampeon club = club { cantTitulos = cantTitulos club + 1} -- esta línea y la de arriba son lo mismo
+
+-- bla bla bla 
+
